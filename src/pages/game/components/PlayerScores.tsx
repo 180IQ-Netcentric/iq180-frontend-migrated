a597ab2e9cd288fbff56fe4e141d6c5f6df8a960
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2021-10-27 17:22:56 +0700

    wip: fix eslint

--- src/pages/game/components/PlayerScores.tsx
+++ src/pages/game/components/PlayerScores.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { PlayerGameInfo } from "../../../contexts/socketContext";
+import React from 'react'
+import { PlayerGameInfo } from '../../../contexts/socketContext'
 
 type Props = {
-  player1: PlayerGameInfo;
-  player2: PlayerGameInfo;
-};
+  player1: PlayerGameInfo
+  player2: PlayerGameInfo
+}
 
 const PlayerScores = (props: Props) => {
-  const { player1, player2 } = props;
+  const { player1, player2 } = props
   // if (!player1 || !player2) return
   return (
-    <div className="playerScore">
-      <div className="player-score-container">
-        <div className="player-1-name">
-          <div className="player-1-logo">{player1.username.charAt(0)}</div>
-          <p className="player-score-name">{player1.username}</p>
-          <div className="player-1-score">{player1.score}</div>
+    <div className='playerScore'>
+      <div className='player-score-container'>
+        <div className='player-1-name'>
+          <div className='player-1-logo'>{player1.username.charAt(0)}</div>
+          <p className='player-score-name'>{player1.username}</p>
+          <div className='player-1-score'>{player1.score}</div>
         </div>
-        <div className="player-2-name">
-          <p className="player-score-name">{player2.username}</p>
-          <div className="player-2-logo">{player2.username.charAt(0)}</div>
-          <div className="player-2-score">{player2.score}</div>
+        <div className='player-2-name'>
+          <p className='player-score-name'>{player2.username}</p>
+          <div className='player-2-logo'>{player2.username.charAt(0)}</div>
+          <div className='player-2-score'>{player2.score}</div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PlayerScores;
+export default PlayerScores
--- src/pages/game/components/RoundEnd.tsx
+++ src/pages/game/components/RoundEnd.tsx
@@ -1,66 +1,66 @@
-import React, { useContext } from "react";
-import { useTranslation } from "react-i18next";
-import { PlayerGameInfo, SocketContext } from "../../../contexts/socketContext";
+import React, { useContext } from 'react'
+import { useTranslation } from 'react-i18next'
+import { PlayerGameInfo, SocketContext } from '../../../contexts/socketContext'
 
 type Props = {
-  player1: PlayerGameInfo;
-  player2: PlayerGameInfo;
-  winnerUsername?: string;
-};
+  player1: PlayerGameInfo
+  player2: PlayerGameInfo
+  winnerUsername?: string
+}
 export const RoundEnd = (props: Props) => {
-  const { winnerUsername } = props;
-  const { t } = useTranslation();
-  const { gameInfo } = useContext(SocketContext);
+  const { winnerUsername } = props
+  const { t } = useTranslation()
+  const { gameInfo } = useContext(SocketContext)
 
-  if (!gameInfo) return null;
-  const { player1, player2 } = gameInfo;
+  if (!gameInfo) return null
+  const { player1, player2 } = gameInfo
 
   const winnerName = (player1: PlayerGameInfo, player2: PlayerGameInfo) => {
-    if (player1.timeUsed === player2.timeUsed) return null;
-    else if (player1.timeUsed < player2.timeUsed) return player1.username;
-    else return player2.username;
-  };
+    if (player1.timeUsed === player2.timeUsed) return null
+    else if (player1.timeUsed < player2.timeUsed) return player1.username
+    else return player2.username
+  }
 
   if (!gameInfo.setting.isClassicMode)
     return (
-      <div className="round-end">
-        <div className="show-winner">
+      <div className='round-end'>
+        <div className='show-winner'>
           <div>
             {!winnerUsername && (
-              <div style={{ fontSize: "36px" }}>{t("70")}</div>
+              <div style={{ fontSize: '36px' }}>{t('70')}</div>
             )}
             {winnerUsername && (
               <div>
-                <div style={{ fontSize: "24px" }}>{t("43")}</div>
-                <div style={{ fontSize: "36px" }}>{winnerUsername}</div>
+                <div style={{ fontSize: '24px' }}>{t('43')}</div>
+                <div style={{ fontSize: '36px' }}>{winnerUsername}</div>
               </div>
             )}
           </div>
         </div>
       </div>
-    );
+    )
 
   return (
-    <div className="round-end">
-      <span style={{ fontSize: "24px" }}>{t("42")}</span>
-      <div className="game-result">
-        <div className="player-name">{player1.username}</div>
-        <div className="score-value">{player1.timeUsed}</div>
+    <div className='round-end'>
+      <span style={{ fontSize: '24px' }}>{t('42')}</span>
+      <div className='game-result'>
+        <div className='player-name'>{player1.username}</div>
+        <div className='score-value'>{player1.timeUsed}</div>
       </div>
-      <div className="game-result">
-        <div className="player-name">{player2.username}</div>
-        <div className="score-value">{player2.timeUsed}</div>
+      <div className='game-result'>
+        <div className='player-name'>{player2.username}</div>
+        <div className='score-value'>{player2.timeUsed}</div>
       </div>
       <hr />
-      <div className="show-winner">
+      <div className='show-winner'>
         <div>
           {winnerName(player1, player2) === null && (
-            <div style={{ fontSize: "36px" }}>{t("70")}</div>
+            <div style={{ fontSize: '36px' }}>{t('70')}</div>
           )}
           {winnerName(player1, player2) !== null && (
             <div>
-              <div style={{ fontSize: "24px" }}>{t("43")}</div>
-              <div style={{ fontSize: "36px" }}>
+              <div style={{ fontSize: '24px' }}>{t('43')}</div>
+              <div style={{ fontSize: '36px' }}>
                 {winnerName(player1, player2)}
               </div>
             </div>
@@ -68,5 +68,5 @@ export const RoundEnd = (props: Props) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
